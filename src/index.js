b57001c74cd414ce55d78ef73b0f0b8f0ceecb67
Larry Jung <jhj46456@gmail.com>
2020-03-24 16:30:11 +0900

    Store and Reducer
    데이터를 저장하는 곳 => Store
    'createStore()'으로 저장소를 만든다.
    이 함수의 인자를 빈 상태로 두면 "reducer는 function이어야 한다"라는 에러가 나온다.
    
    reducer function을 만들어서 'createStore()'의 인자에 넣으면 된다.
    이 reducer는 첫번째 인자에 state, 두번째 인자에 action을 받는다.
    
    createStore(reducer, data:Object) >>> 두번째 인자로 들어간 Object가
    reducer의 첫번째 인자인 state로 들어간다.
    
    reducer는 state를 변경하는 것이 아닌 대체하는 것이다.
    
    store에 저장된 data(state)는 reducer를 통해서만 접근할 수 있다.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = state => {
+  return state;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+const countStore = createStore(countModifier, { state: 0 });
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
