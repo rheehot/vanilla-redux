bbbbbdd2f84b6e440ef3d6ddd3f8b745d1bc87c5
Larry Jung <jhj46456@gmail.com>
2020-03-28 18:43:46 +0900

    Delete

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addToDo, actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
 function Home({ toDos, addToDo }) {
-  console.log({ toDos });
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -20,7 +20,11 @@ function Home({ toDos, addToDo }) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul>{JSON.stringify(toDos)}</ul>
+      <ul>
+        {toDos.map(toDo => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
     </>
   );
 }
