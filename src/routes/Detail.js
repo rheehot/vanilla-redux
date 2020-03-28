49011a905fa4070ab8cbb26d5ee51a779dbe823d
Larry Jung <jhj46456@gmail.com>
2020-03-28 18:54:35 +0900

    Detail screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,22 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+const Detail = ({ toDo }) => {
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const {
+    match: {
+      params: { id }
+    }
+  } = ownProps;
+  return { toDo: state.find(item => item.id === Number(id)) };
+};
+
+export default connect(mapStateToProps)(Detail);
